c0fcee3698459a77106b416b58fc459547f4e8ba
Francesco Abbate <Francesco.Abbate@nice.com>
2014-11-20 19:12:06 +0000

    saving current state

--- app/src/main/java/com/technologyday/ciccio/techdayandroidapp/MainActivity.java
+++ app/src/main/java/com/technologyday/ciccio/techdayandroidapp/MainActivity.java
@@ -1,17 +1,34 @@
 package com.technologyday.ciccio.techdayandroidapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello Ciccio Android App!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 
     @Override
@@ -38,8 +55,6 @@ public class MainActivity extends Activity {
             startActivity(intent);
             return true;
         }
-
-
         return super.onOptionsItemSelected(item);
     }
 }
